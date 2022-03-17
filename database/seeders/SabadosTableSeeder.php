c4f9c64048baf22b43c037883c68c180f4f27142
JoseElyabe <josefasales9@gmail.com>
2022-03-17 11:04:51 -0300

    Correção do nome da pasta SEEDERS

---
+++ database/seeders/SabadosTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Http\Models\SabadoLetivo;
+
+class SabadosTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+         SabadoLetivo::create([
+            'referente'     => 'SEXTA',
+            'data'          => "2018-08-18"
+        ]);
+
+         SabadoLetivo::create([
+            'referente'     => 'SEGUNDA',
+            'data'          => "2018-10-06"
+        ]);
+    }
+}
