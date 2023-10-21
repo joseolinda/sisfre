193d9506f4ff7629645333831b2175d19f1cd0fc
Jose Olinda <joseolindad@yahoo.com.br>
2023-10-21 08:37:35 -0300

    Trying fix view.compiled path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Make sure the directory for compiled views exist 
+        /*
         if (! is_dir(config('view.compiled'))) { 
             mkdir(config('view.compiled'), 0755, true); 
         }
+        */
     }
 
     /**
