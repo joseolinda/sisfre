d49ee4d4ff2b169663d893ef218fe2b772e78bfb
Jose Olinda <joseolindad@yahoo.com.br>
2023-10-22 10:22:16 -0300

    Set mime type of asset

--- api/assets.php
+++ api/assets.php
@@ -3,4 +3,32 @@
 // Purpose: serve assets from public folder
 $file = $_SERVER['REQUEST_URI'];
 
+// get the file extension
+$ext = pathinfo($file, PATHINFO_EXTENSION);
+
+// set mime type based on extension
+switch ($ext) {
+    case 'css':
+        $mime = 'text/css';
+        break;
+    case 'js':
+        $mime = 'text/javascript';
+        break;
+    case 'png':
+        $mime = 'image/png';
+        break;
+    case 'jpg':
+        $mime = 'image/jpeg';
+        break;
+    case 'gif':
+        $mime = 'image/gif';
+        break;
+    default:
+        $mime = 'text/plain';
+        break;
+}
+
+// set the content type header
+header('Content-Type: ' . $mime);
+
 require __DIR__ . '/../public' . $file;
